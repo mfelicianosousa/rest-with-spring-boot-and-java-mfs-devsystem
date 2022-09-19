1faeb3156d9a8dd2aa344d049959e41bfcc378b1
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-09-19 17:30:08 -0400

    Migrations with Flyway

---
+++ 11-MigrationsWithFlyWay/rest-with-spring-boot-and-java-mfs-devsystem/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `person` (`address`, `first_name`, `gender`, `last_name`) VALUES
+	('Cuiabá-MT-Brasil', 'Marcelino', 'Male', 'Sousa'),
+	('Cuiabá-MT', 'Ana Paula', 'fema', 'Santos'),
+	('Cuiabá-MT', 'Pedro', 'Male', 'Santos'),
+	('Porbadar-India', 'Indira', 'Female', 'Gandhi'),
+	('Porbandar - India', 'Mahatma', 'Male', 'Gandhi'),
+	('Kentucky - US', 'Muhammad', 'Male', 'Ali'),
+	('Muezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	('Smilja - Croatia', 'Nikola', 'Male', 'Tesla');
