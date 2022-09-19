3eda0bcb67ce54f5e7cb79e22450a7af778db392
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-09-19 19:51:08 -0400

    ContentNegotiation

---
+++ 13_ContentNegociation/rest-with-spring-boot-and-java-mfs-devsystem/src/main/java/com/mfsdevsystem/repositories/PersonRepository.java
@@ -0,0 +1,11 @@
+package com.mfsdevsystem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mfsdevsystem.model.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long>{
+
+}
